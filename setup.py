72ebfcbf586ab353d510afc47367f910a35bbcee
T. Rogers <tr7200@users.noreply.github.com>
2020-05-23 10:29:55 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="Youdens_index",
+      version="0.1",
+      description='Tensorflow-Keras callback implementing Kaivanto (2008)',
+      url='http://github.com/tr7200/Youdens_index',
+      license='MIT',
+      packages=["Youdens_index"])
